c0ba5435c75056dbc9d3ac04165d7081017666ff
michael <maydev99@gmail.com>
2021-11-28 18:15:32 -0500

    Added decrement button and functionality in second scree appbar

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,14 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/foundation.dart';
-
 
 class Counter with ChangeNotifier, DiagnosticableTreeMixin {
   int _count = 0;
+
   int get count => _count;
 
   void increment() {
     _count++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _count--;
     notifyListeners();
   }
 
@@ -19,46 +24,52 @@ class Counter with ChangeNotifier, DiagnosticableTreeMixin {
   }
 }
 
-
 class SecondPage extends StatefulWidget {
   const SecondPage({Key? key}) : super(key: key);
-
 
   @override
   _SecondPageState createState() => _SecondPageState();
 }
 
 class _SecondPageState extends State<SecondPage> {
-
- @override
+  @override
   void initState() {
     MultiProvider(
       providers: [
-       ChangeNotifierProvider(create: (_) => Counter()),
+        ChangeNotifierProvider(create: (_) => Counter()),
       ],
     );
     super.initState();
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-         context.read<Counter>().increment();
+          context.read<Counter>().increment();
         },
+        heroTag: 'increment',
         child: const Icon(Icons.add),
       ),
       appBar: AppBar(
         title: const Text('Second Screen'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                context.read<Counter>().decrement();
+              },
+              icon: const Icon(Icons.remove))
+        ],
       ),
       body: Center(
-        child: Text('${context.watch<Counter>().count}',
+        child: Text(
+          '${context.watch<Counter>().count}',
           style: const TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 80,
-          ),),
+          ),
+        ),
       ),
     );
   }
